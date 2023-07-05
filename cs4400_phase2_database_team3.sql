2558697c51e0b613370f1b9b4367a7a79159a696
Pau Sum <spausum@outlook.com>
2023-07-05 15:55:15 +0200

    Create database and airport table

--- cs4400_phase2_database_team3.sql
+++ cs4400_phase2_database_team3.sql
@@ -1,1 +1,11 @@
+DROP DATABASE IF EXISTS sams;
+CREATE DATABASE IF NOT EXISTS sams;
+USE sams;
 
+CREATE TABLE 'Airport' (
+    'airportID' char(3),
+    'name' varchar(),
+    'city' varchar(),
+    'state' varchar(),
+    PRIMARY KEY 
+)
