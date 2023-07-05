a9d56ef73398c820154f6a16d9b308c47ff854ea
Pau Sum <spausum@outlook.com>
2023-07-05 17:03:34 +0200

    Create person, pilot, license, passenger, airline, airplane, location tables

--- cs4400_phase2_database_team3.sql
+++ cs4400_phase2_database_team3.sql
@@ -14,8 +14,12 @@ CREATE TABLE airport (
 DROP TABLE IF EXISTS leg;
 CREATE TABLE leg (
     legID char(50),
+    departs char(3),
+    arrives char(3),
     distance smallint,
-    PRIMARY KEY (legID)  
+    PRIMARY KEY (legID),
+    FOREIGN KEY (departs) REFERENCES airport (airportID),
+	FOREIGN KEY (arrives) REFERENCES airport (airportID)
 );
 
 DROP TABLE IF EXISTS route;
@@ -27,24 +31,103 @@ CREATE TABLE route (
 DROP TABLE IF EXISTS contain_of;
 CREATE TABLE contain_of (
     sequence char(50),
-    PRIMARY KEY (sequence)  
+    legID char(50),
+    routeID char(50),
+    PRIMARY KEY (sequence),
+    FOREIGN KEY (legID) REFERENCES leg (legID),
+	FOREIGN KEY (routeID) REFERENCES route (routeID)
 );
 
 DROP TABLE IF EXISTS flight;
 CREATE TABLE flight (
     flightID char(50),
-    PRIMARY KEY (flightID)  
+    routeID char(50),
+    PRIMARY KEY (flightID),
+    FOREIGN KEY (routeID) REFERENCES route (routeID)
 );
 
 DROP TABLE IF EXISTS ticket;
 CREATE TABLE ticket (
     ticketID char(50),
     cost smallint,
-    PRIMARY KEY (ticketID)  
+    offers char(50),
+    deplanes char(3),
+    PRIMARY KEY (ticketID),
+    FOREIGN KEY (offers) REFERENCES flight (flightID),
+    FOREIGN KEY (deplanes) REFERENCES airport (airportID)
 );
 
 DROP TABLE IF EXISTS seat;
 CREATE TABLE seat (
     seat_num char(3),
-    PRIMARY KEY (seat_num)  
+    ticketID char(50),
+    PRIMARY KEY (seat_num),
+    FOREIGN KEY (ticketID) REFERENCES ticket (ticketID)
 );
+
+DROP TABLE IF EXISTS person;
+CREATE TABLE person (
+    personID char(50),
+    f_name char(100),
+    l_name char(100),
+    PRIMARY KEY (personID)  
+);
+
+DROP TABLE IF EXISTS pilot;
+CREATE TABLE pilot (
+    personID char(50),
+    taxID char(100),
+    PRIMARY KEY (personID),
+    UNIQUE KEY taxID (taxID),
+    FOREIGN KEY (personID) REFERENCES person (personID)
+);
+
+DROP TABLE IF EXISTS licence;
+CREATE TABLE licence (
+    licence_type char(50),
+    pilotID char(50),
+    PRIMARY KEY (licence_type, pilotID),
+    FOREIGN KEY (pilotID) REFERENCES pilot (personID)
+);
+
+DROP TABLE IF EXISTS passenger;
+CREATE TABLE passenger (
+    personID char(50),
+    miles int,
+    PRIMARY KEY (personID),
+    FOREIGN KEY (personID) REFERENCES person (personID)
+);
+
+DROP TABLE IF EXISTS airline;
+CREATE TABLE airline (
+    airlineID char(50),
+    revenue float,
+    PRIMARY KEY (airlineID)
+);
+
+DROP TABLE IF EXISTS airplane;
+CREATE TABLE airplane (
+    tail_num char(50),
+    owned_by char(50),
+    speed smallint,
+    seat_cap smallint,
+    plane_type char(50),
+    prop_count tinyint,
+    skid_count tinyint,
+    engine_count tinyint,
+    PRIMARY KEY (tail_num, owned_by),
+    FOREIGN KEY (owned_by) REFERENCES airline (airlineID)
+);
+
+DROP TABLE IF EXISTS location;
+CREATE TABLE location (
+    locID char(50),
+    airportID char(50),
+    tail_num char(50),
+    owned_by char(50),
+    PRIMARY KEY (locID),
+    FOREIGN KEY (airportID) REFERENCES airport (airportID),
+    FOREIGN KEY (tail_num, owned_by) REFERENCES airplane (tail_num, owned_by)
+);
+
+
